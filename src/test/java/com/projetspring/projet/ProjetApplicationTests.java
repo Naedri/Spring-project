e8c48ed44b321f29802fc4470a09a6f2308017d4
reifocs <alainvincentesc@gmail.com>
2021-03-18 08:17:44 +0100

    initial commit

---
+++ src/test/java/com/projetspring/projet/ProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetspring.projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
