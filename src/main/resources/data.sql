118f3a03465e0744502f216952229eb52d5b014f
reifocs <alainvincentesc@gmail.com>
2021-03-19 09:46:43 +0100

    feat: sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO actors
+values (1, 'Jean', 'Dujardin');
+INSERT INTO actors
+values (2, 'Gerard', 'Depardieu');
+INSERT INTO actors
+values (3, 'Tom', 'Cruise');
+
+INSERT INTO movies
+values (1, 5, 'Un film sur des espions', 'OSS 117');
+INSERT INTO movies
+values (2, 4, 'Un film sur des gaulois', 'Asterix & Obelix');
+INSERT INTO movies
+values (3, 3, 'Un film sur des missions pas faciles', 'Mission impossible');
+
+INSERT INTO plays
+values (1, 1);
+INSERT INTO plays
+values (2, 2);
+INSERT INTO plays
+values (3, 3);
